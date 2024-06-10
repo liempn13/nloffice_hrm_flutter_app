827e0a67d22241dfc4b711bbaa5b025ca0ba55a4
Nhutlam <nhutlam0000@gmail.com>
2024-06-10 10:23:38 +0700

    bo sung file main

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hrm_app/views/screen/Token.dart';
+import 'package:hrm_app/views/widgets/bottom_nav_controller.dart';
+import 'package:hrm_app/views/widgets/chip_textfield_list.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyDismissibleList(),
+        ),
+        bottomNavigationBar: BottomNavController(),
+      ),
+    );
+  }
+}
